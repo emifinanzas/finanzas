ab26cfe1be54febb6be3bcb3b2509fb6ec56403b
emifinanzas <fernandezemilianojulian@gmail.com>
2026-04-23 16:47:00 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,32 +1,31 @@
-const CACHE = 'finanzas-v3';
+const CACHE = 'finanzas-v2';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 
-self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
+  self.skipWaiting();
+});
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
-  const url = new URL(e.request.url);
-  if (url.hostname.includes('google') || url.hostname.includes('cdnjs')) return;
-  const isPage = url.pathname.endsWith('.html') || url.pathname.endsWith('/') || url.pathname.endsWith('/finanzas');
-  if (isPage) {
-    e.respondWith(
-      fetch(e.request).then(res => {
-        caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+  if (e.request.url.includes('googleapis.com') || e.request.url.includes('accounts.google.com')) return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
         return res;
-      }).catch(() => caches.match(e.request))
-    );
-  } else {
-    e.respondWith(
-      caches.match(e.request).then(c => c || fetch(e.request).then(res => {
-        caches.open(CACHE).then(cache => cache.put(e.request, res.clone()));
-        return res;
-      }))
-    );
-  }
+      });
+    }).catch(() => caches.match('./index.html'))
+  );
 });
